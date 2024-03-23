cd88355df94a10fc1a5a64a51770d8e1c9a1618b
Ali Mansour <alialhadimansour21@gmail.com>
2024-03-23 15:52:05 +0200

    extract a component

--- src/App.js
+++ src/App.js
@@ -1,77 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
+import Counter from "./Counter";
 
-const App = ({ value, isRoot, onRemove, parentRemove }) => {
-  const [subOptions, setSubOptions] = useState([]);
-
-  const handleAddSubOption = () => {
-    const newValue = subOptions.length === 0 ? value : value + 1;
-    setSubOptions((prevSubOptions) => [
-      ...prevSubOptions,
-      <App
-        key={newValue}
-        value={newValue}
-        onRemove={() => handleRemoveChild(newValue)}
-        parentRemove={onRemove}
-      />,
-    ]);
-  };
-
-  const handleRemoveChild = (childValue) => {
-    setSubOptions((prevSubOptions) =>
-      prevSubOptions.filter((option) => option.props.value !== childValue)
-    );
-  };
-
-  const handleRemoveParent = () => {
-    if (isRoot) {
-      onRemove();
-    } else {
-      parentRemove(value);
-    }
-  };
-
-  return (
-    <div style={{ paddingLeft: 15 }}>
-      <span>-{value}</span>
-      <button className="increment" onClick={handleAddSubOption}>
-        +
-      </button>
-      <button className="decrement" onClick={handleRemoveParent}>
-        -
-      </button>
-      {subOptions}
-    </div>
-  );
-};
-
-const MainApp = () => {
+const App = () => {
   const [value] = useState(1);
-  const [isMainAppVisible, setIsMainAppVisible] = useState(true);
-  const [subOptions, setSubOptions] = useState([]);
-
-  const handleRemoveMain = () => {
-    setIsMainAppVisible(false);
-  };
-
-  const handleRemoveChild = (childValue) => {
-    setSubOptions((prevSubOptions) =>
-      prevSubOptions.filter((option) => option.props.value !== childValue)
-    );
-  };
-
   return (
     <div>
-      {isMainAppVisible && (
-        <App
-          value={value}
-          isRoot
-          onRemove={handleRemoveMain}
-          parentRemove={handleRemoveChild}
-        />
-      )}
+      <Counter value={value} />
     </div>
   );
 };
 
-export default MainApp;
+export default App;
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 
-const Counter = ({ options }) => {
+const Counter = ({ value }) => {
+  const [subOptions, setSubOptions] = useState([]);
+
+  const handleAddSubOption = () => {
+    const newValue =
+      subOptions.length === 0 ? value : value + subOptions.length;
+    setSubOptions((prevSubOptions) => [
+      ...prevSubOptions,
+      <Counter key={newValue} value={newValue} />,
+    ]);
+  };
+
   return (
-    <ul>
-      {options.map((option, index) => {
-        <li key={index}>
-          <Counter options={option.subOptions} />
-        </li>;
-      })}
-    </ul>
+    <div style={{ paddingLeft: 15 }}>
+      <span>-{value}</span>
+      <button className="increment" onClick={handleAddSubOption}>
+        +
+      </button>
+      <button className="decrement">-</button>
+      {subOptions}
+    </div>
   );
 };
 
